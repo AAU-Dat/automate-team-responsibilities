08d2453ace061630fa720f66141e921925a4142f
Daniel Runge Petersen <rungedaniel@hotmail.com>
2021-10-06 19:44:49 +0200

    small fixes

--- helpers/discordFunctions.js
+++ helpers/discordFunctions.js
@@ -7,7 +7,7 @@ const AAU_ICON =
     "https://media-exp1.licdn.com/dms/image/C510BAQHgfIxjqWSSsQ/company-logo_200_200/0/1519855922416?e=2159024400&v=beta&t=_L2XLMCXeEzSX_c8oBMM_uuEK1kgfno7ViFJXD5BP6U";
 const ORGANIZATION = "https://github.com/AAU-Dat";
 const OCTOCAT =
-    "https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667";
+    "https://api.iconify.design/octicon/mark-github-16.svg";
 
 function breakDiscordWebhook(webhook) {
     const array = webhook.split("/");
