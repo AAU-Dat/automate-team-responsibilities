052cd6b35ab1e0d464b2a976fdcb3053c31cd110
Daniel Runge Petersen <rungedaniel@hotmail.com>
2021-10-06 19:36:54 +0200

    small fixes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const { buildDiscordEmbed, postToDiscord } = require("./helpers/discordFunctions");
 const { partiallyRandomDistribution } = require("./helpers/distributions")
 
@@ -9,21 +8,13 @@ try {
   const responsibilities = JSON.parse(core.getInput('responsibilities'));
   const discordWebhook = core.getInput('discord-webhook');
 
-  console.log(JSON.stringify(team));
-  console.log(JSON.stringify(responsibilities));
-
   const distribution = partiallyRandomDistribution(team, responsibilities);
-
-  console.log(JSON.stringify(distribution));
   const embed = buildDiscordEmbed(distribution);
 
   postToDiscord(embed, discordWebhook);
 
   core.setOutput("responsibility-distribution", JSON.stringify(distribution));
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
